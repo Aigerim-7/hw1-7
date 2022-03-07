6327ecc7d4889b36805254daea41e6b26b5109cc
Your Name <aigerimryskulova071001@gmail.com>
2022-03-07 18:23:03 +0600

    hw

---
+++ movie_app/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Director(models.Model):
+    name = models.CharField(max_length=100)
+
+    @property
+    def movies_count(self):
+        return self.director.all().count()      
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100, null=True)
+    description = models.TextField()
+    duration = models.CharField( max_length=100, null=True, blank=True)
+    director = models.ForeignKey(Director, on_delete=models.DO_NOTHING, related_name='director')
+
+    @property
+    def rating(self):
+        reviews = Review.objects.filter(movie=self)
+        stars = [ i.stars for i in reviews ]
+        if len(stars)>0:
+            return sum(stars)/len(stars)
+        elif len(stars)==0:
+            return stars
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    text = models.TextField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='review')
+    stars = models.PositiveIntegerField(default=5, validators=[MinValueValidator(1), MaxValueValidator(5)], null=True)
+
+    def __str__(self):
+        return f"Review: {self.movie.title}"
+        
+
+class VerifyToUser(models.Model):
+    code = models.CharField(max_length=100)
---
+++ movie_app/serializers.py
@@ -0,0 +1,98 @@
+from rest_framework.exceptions import ValidationError
+from rest_framework.serializers import SerializerMethodField
+from rest_framework import serializers
+from . import models
+from django.contrib.auth.models import User
+
+class DirectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Director
+        fields = [
+            "id",
+            "name",
+            "movies_count",
+        ]
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Review
+        fields = [
+            "id",
+            "text",
+            "movie",
+            "stars",
+        ]
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Movie
+        fields = [
+            "id",
+            "title",
+            "description",
+            "duration",
+            "director",
+        ]
+
+class ReviewMovieSerializer(serializers.ModelSerializer):
+    director = SerializerMethodField()
+
+    review = SerializerMethodField()
+
+    class Meta:
+        model = models.Movie
+        fields = [
+            "id",
+            "title",
+            "description",
+            "duration",
+            "director",
+            "review",
+            "rating"
+        ]
+    def get_director(self, direc):
+        try:
+            return direc.director.name
+        except:
+            return "Пусто!"
+
+
+    def get_review(self, rev):
+        serializer = ReviewSerializer(rev.review.filter(text__isnull=False), many=True)
+        return serializer.data
+
+class DirectorCreateUpdateSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+
+class ReviewCreateUpdateSerailizer(serializers.Serializer):
+    text = serializers.CharField()
+    movie_id = serializers.IntegerField()
+    stars = serializers.IntegerField(min_value=1, max_value=5)
+
+    def validate_movie_id(self, movie_id):
+        if models.Movie.objects.filter(id=movie_id).count()==0:
+            raise ValidationError(f'id {movie_id} does not exist')
+
+class MovieCreateUpdateSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=100)
+    description = serializers.CharField()
+    duration = serializers.CharField(max_length=100)
+    director_id = serializers.IntegerField()
+
+    def validate_director_id(self, director_id):
+        if models.Director.objects.filter(id=director_id).count()==0:
+            raise ValidationError(f'id {director_id} does not exist')
+            
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate_username(self, username):
+        if  User.objects.filter(username=username):
+            raise ValidationError(f"User with this username already exist!")
+        return username
+
+class AuthorizateSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
